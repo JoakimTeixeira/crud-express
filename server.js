c7d93ecfb494046743a67fbaa9e15d4082e40a17
Joakim Teixeira <setokim1@hotmail.com>
2020-08-01 23:51:11 -0300

    Add handlebars middleware

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import express from "express";
 import { router } from "./routes/members.mjs";
+import exphbs from "express-handlebars";
 
 const app = express();
 app.use(express.json());
 app.use("/api/members", router);
+
+// Express handlebars middleware
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+// Homepage route
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "Member list",
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
